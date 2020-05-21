bf0d8a0d151d233b11604be3a8a294e5f3f6dec1
naydyonov <naydyonov@etm-system.ru>
2020-05-21 09:47:09 +0300

    script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
 {
+  const menuIcon = document.querySelector('.header__menu-icon');
+  const mobileMenu = document.querySelector('.header__mobile-menu');
+  const body = document.querySelector('body');
+  
+  
   const menuItems = document.querySelectorAll('.menu__item_sub');
   menuItems.forEach(item => {
     item.addEventListener('click', () => {
-      item.classList.add('open');
+      event.stopPropagation();
+      item.classList.toggle('open');
+      console.log('click on .menu__item_sub');
     });
   });
 
@@ -10,23 +17,46 @@
   
   submenuItems.forEach(item => {
     item.addEventListener('click', () => {
-      item.closest('.menu__item_sub').classList.remove('open');
       event.stopPropagation();
+      item.closest('.menu__item_sub').classList.remove('open');
+      if (item.closest('.header__mobile-menu')) {
+        mobileMenu.classList.toggle('active');
+      }
+      console.log('click on .menu__item_sub .menu__item');
     });
   });
 
-  const menuIcon = document.querySelector('.header__menu-icon');
-  const mobileMenu = document.querySelector('.header__mobile-menu');
-  const body = document.querySelector('body');
+  
   menuIcon.addEventListener('click', () => {
+    event.stopPropagation();
     mobileMenu.classList.toggle('active');
-
     if (mobileMenu.classList.contains('active')) {
       body.classList.add('overflow');
     } else {
       body.classList.remove('overflow');
     }
+  });
 
+
+  document.addEventListener('click', () => {
+    const target = event.target;
+    const isMobileMenu = target.closest('.header__mobile-menu');
+    const isMenuItem = target.classList.contains('menu__item') || target.closest('.menu__item');
+    const isSubmenuItem = target.classList.contains('menu__item_sub') || target.parentNode.parentNode.classList.contains('menu__item_sub');
+    const menu = target.closest('.menu');
+    
+    if (isMobileMenu && isMenuItem && !isSubmenuItem) {
+      mobileMenu.classList.remove('active');
+    }
+
+    if (!menu) {
+      menuItems.forEach(item => {
+        item.classList.remove('open');
+      })
+      mobileMenu.classList.remove('active');
+    }
+    console.log('Global Event');
+    
   });
 
 }
